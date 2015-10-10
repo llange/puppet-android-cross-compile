3206a0e52898105f4253ac5e149612cba248c9c9
Ludovic LANGE <github@lange.nom.fr>
2015-10-10 13:52:25 +0200

    Fix for warning allow_virtual_packages

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,12 @@
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
+
 define puppetfile($mode = 0644, $owner = "root", $group = "root") {
   file {
     $title:
